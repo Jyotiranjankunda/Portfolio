4ee68d66dcb03d57ecbdec8a4626e03502a374b4
Jyotiranjan Kunda <jrk2001.mymail@gmail.com>
2024-05-20 20:52:18 +0530

    About

--- src/components/aboutme/Aboutme.jsx
+++ src/components/aboutme/Aboutme.jsx
@@ -5,7 +5,13 @@ const Aboutme = () => {
     <>
       <div className={classes.dummy} id='dummy-about'></div>
       <div className={classes.aboutme} id='aboutme'>
-        Aboutme
+        <p className={classes.whoami}>
+          <p>WHO AM I ?</p>
+        </p>
+        <p className={classes.heading}>About Me</p>
+        <p>
+        Hello! I&apos;m Jyotiranjan Kunda, a passionate software engineer with a knack for problem-solving and innovation. 🚀 Proficient in languages like C/C++, Java, and JavaScript, I enjoy crafting seamless user experiences and optimizing performance. With a strong academic background and a penchant for continuous learning, I excel in various tech stacks and developer tools. Beyond coding, I'm an avid traveler 🌍, fitness enthusiast 💪, and always on a path of self-improvement 📈. Let's connect and explore new possibilities together! 🚀
+        </p>
       </div>
     </>
   );
