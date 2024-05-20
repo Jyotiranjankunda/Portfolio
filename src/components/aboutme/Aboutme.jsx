bf81dd412aaa87f1d745c301e913089618de2bdd
Jyotiranjan Kunda <jrk2001.mymail@gmail.com>
2024-05-21 00:25:50 +0530

    Skill section completed

--- src/components/aboutme/Aboutme.jsx
+++ src/components/aboutme/Aboutme.jsx
@@ -1,17 +1,108 @@
 import classes from './aboutme.module.css';
+import aboutPhoto from '../../assets/aboutphoto.png';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { GiNewBorn } from 'react-icons/gi';
+import { FaSchool } from 'react-icons/fa6';
+import { BiSolidSchool } from 'react-icons/bi';
+import { MdWork } from 'react-icons/md';
 
 const Aboutme = () => {
   return (
     <>
       <div className={classes.dummy} id='dummy-about'></div>
       <div className={classes.aboutme} id='aboutme'>
-        <p className={classes.whoami}>
-          <p>WHO AM I ?</p>
-        </p>
         <p className={classes.heading}>About Me</p>
-        <p>
-        Hello! I&apos;m Jyotiranjan Kunda, a passionate software engineer with a knack for problem-solving and innovation. 🚀 Proficient in languages like C/C++, Java, and JavaScript, I enjoy crafting seamless user experiences and optimizing performance. With a strong academic background and a penchant for continuous learning, I excel in various tech stacks and developer tools. Beyond coding, I'm an avid traveler 🌍, fitness enthusiast 💪, and always on a path of self-improvement 📈. Let's connect and explore new possibilities together! 🚀
-        </p>
+        <div className={classes.sections}>
+          <div className={classes.leftSection}>
+            <img src={aboutPhoto} alt='' />
+            <div className={classes.about}>
+              <p>
+                Hello! I&apos;m Jyotiranjan Kunda, a simple yet ambitious soul with a zest
+                for life. Born in Odisha and raised in the serene town of Haridwar since my childhood, I&apos;ve grown to appreciate the beauty of nature and the
+                value of close-knit communities. Whether it&apos;s cycling through scenic
+                routes or indulging in fitness routines, I find joy in staying active and
+                embracing the great outdoors. 🌳🚴‍♂️
+              </p>
+              <p>
+                When I&apos;m not immersed in the world of technology, you&apos;ll find me
+                enjoying the simpler pleasures of life. I love listening to songs,
+                watching movies, and spending quality time with family and friends. My
+                interests also extend to playing indoor games like carrom, chess, and
+                table tennis, as well as outdoor sports like cricket and badminton. These
+                activities keep me balanced and energized. 🏏♟️
+              </p>
+              <p>
+                In my quiet moments, I dive into the world of books, reading novels and
+                biographies that inspire and broaden my horizons. With high aspirations
+                and a passion for continuous growth, I strive to lead a fulfilling and
+                enriching life. Let&apos;s connect and share the wonders of our journeys!
+                📚✨
+              </p>
+            </div>
+          </div>
+          <div className={classes.rightSection}>
+            <VerticalTimeline>
+              <VerticalTimelineElement
+                className='vertical-timeline-element--work'
+                contentStyle={{ background: '#C8FACD', color: '#005249' }}
+                iconStyle={{ background: '#C8FACD', color: '#005249' }}
+                icon={<GiNewBorn />}>
+                <h3 className='vertical-timeline-element-title'>Born On: </h3>
+                <h4 className='vertical-timeline-element-subtitle'>
+                  30<sup>th</sup> October, 2001
+                </h4>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className='vertical-timeline-element--work'
+                iconStyle={{ background: '#C8FACD', color: '#005249' }}
+                contentStyle={{ background: '#C8FACD', color: '#005249' }}
+                icon={<FaSchool />}>
+                <h3 className='vertical-timeline-element-title'>Matriculation</h3>
+                <h4 className='vertical-timeline-element-subtitle'>
+                  Angels&apos; Acacdemy Sr. Sec. School (2017)
+                </h4>
+                <p>CGPA: 10/10</p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className='vertical-timeline-element--work'
+                iconStyle={{ background: '#C8FACD', color: '#005249' }}
+                contentStyle={{ background: '#C8FACD', color: '#005249' }}
+                icon={<FaSchool />}>
+                <h3 className='vertical-timeline-element-title'>Intermediate</h3>
+                <h4 className='vertical-timeline-element-subtitle'>
+                  Angels&apos; Acacdemy Sr. Sec. School (2019)
+                </h4>
+                <p>Percentage: 84%</p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className='vertical-timeline-element--work'
+                iconStyle={{ background: '#C8FACD', color: '#005249' }}
+                contentStyle={{ background: '#C8FACD', color: '#005249' }}
+                icon={<BiSolidSchool />}>
+                <h3 className='vertical-timeline-element-title'>College</h3>
+                <h4 className='vertical-timeline-element-subtitle'>
+                  Veer Surendra Sai University of Technology (2024)
+                </h4>
+                <p>B.Tech in CSE (CGPA: 9.04)</p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className='vertical-timeline-element--work'
+                iconStyle={{ background: '#C8FACD', color: '#005249' }}
+                contentStyle={{ background: '#C8FACD', color: '#005249' }}
+                icon={<MdWork />}>
+                <h3 className='vertical-timeline-element-title'>Company</h3>
+                <h4 className='vertical-timeline-element-subtitle'>
+                  Incture Technologies (Jan 2024 - Present)
+                </h4>
+                <p>ASE Trainee</p>
+              </VerticalTimelineElement>
+            </VerticalTimeline>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,11 +1,86 @@
 import classes from './skills.module.css';
+import html from '../../assets/html.png';
+import css from '../../assets/css.png';
+import sass from '../../assets/sass.png';
+import js from '../../assets/js.png';
+import react from '../../assets/react.png';
+import node from '../../assets/node.png';
+import express from '../../assets/express.png';
+import mongodb from '../../assets/mongodb.png';
+import mysql from '../../assets/mysql.png';
+import c from '../../assets/c.png';
+import cpp from '../../assets/cpp.png';
+import java from '../../assets/java.png';
+import git from '../../assets/git.png';
+import github from '../../assets/github.png';
+import postman from '../../assets/postman.png';
+import figma from '../../assets/figma.png';
 
 const Skills = () => {
+  const skillSet = [
+    { name: 'HTML', percentage: 90, img: html },
+    { name: 'CSS', percentage: 85, img: css },
+    { name: 'Sass', percentage: 60, img: sass },
+    { name: 'Javascript', percentage: 80, img: js },
+    { name: 'ReactJs', percentage: 75, img: react },
+    { name: 'NodeJs', percentage: 50, img: node },
+    { name: 'ExpressJs', percentage: 50, img: express },
+    { name: 'MongoDB', percentage: 50, img: mongodb },
+    { name: 'MySQL', percentage: 70, img: mysql },
+    { name: 'C', percentage: 80, img: c },
+    { name: 'CPP', percentage: 90, img: cpp },
+    { name: 'Java', percentage: 50, img: java },
+    { name: 'Git', percentage: 60, img: git },
+    { name: 'GitHub', percentage: 50, img: github },
+    { name: 'Postman', percentage: 50, img: postman },
+    { name: 'Figma', percentage: 50, img: figma },
+  ];
+
   return (
     <>
       <div className={classes.dummy} id='dummy-skills'></div>
       <div className={classes.skills} id='skills'>
-        Skills
+        <section className={classes.skillSection}>
+          <div className={`${classes.header} ${classes.skillHead}`}>
+            <h2 className={classes.heading}>
+              My <span>Skills</span>
+            </h2>
+            <p>Here is my skill set to represent my expertise.</p>
+          </div>
+          {/* <div className={classes.skillMain}>
+            {Object.keys(skillSet).map((item) => (
+              <div className={classes.skillBar} key={item}>
+                <div className={classes.info}>
+                  <p className={classes.skillName}>{item}</p>
+                  <p className={classes.percent}>{skillSet[item]}%</p>
+                </div>
+                <div className={classes.bar}>
+                  <span style={{ width: `${skillSet[item]}%` }}></span>
+                </div>
+              </div>
+            ))}
+          </div> */}
+          <div className={classes.skillMain}>
+            {skillSet.map((skill, index) => (
+              <div className={classes.skillBar} key={index}>
+                <div className={classes.info}>
+                  <div className={classes.logo}>
+                    <img
+                      src={skill.img}
+                      alt={`${skill.name} logo`}
+                      className={classes.skillLogo}
+                    />
+                    <p className={classes.skillName}>{skill.name}</p>
+                  </div>
+                  <p className={classes.percent}>{skill.percentage}%</p>
+                </div>
+                <div className={classes.bar}>
+                  <span style={{ width: `${skill.percentage}%` }}></span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
       </div>
     </>
   );
