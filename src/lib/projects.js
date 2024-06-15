585651fd24872ed9d76032863bc6496b06462e88
Jyotiranjan Kunda <jrk2001.mymail@gmail.com>
2024-06-15 21:23:44 +0530

    changes in project

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,18 +1,27 @@
-import chatninja from "../assets/chatninja.png";
-import youtubeclone from "../assets/youtubeclone.png";
-import wanderworld from "../assets/wanderworld.png";
-import ecommerce from "../assets/ecommerce.png";
-import ludo from "../assets/ludo.png";
-import keyblaze from "../assets/keyblaze.png";
+import expense from '../assets/expense.png';
+import book from '../assets/book.png';
+import youtubeclone from '../assets/youtubeclone.png';
+import wanderworld from '../assets/wanderworld.png';
+import ecommerce from '../assets/ecommerce.png';
+import ludo from '../assets/ludo.png';
+import keyblaze from '../assets/keyblaze.png';
 
 export const PROJECTS = [
   {
     id: 1,
-    image: chatninja,
-    heading: 'Chat Ninja',
-    text: ' A real-time chat application using the MERN stack with secure authentication and instant messaging.',
-    sourceCode: 'https://github.com/Jyotiranjankunda/mern-chat-app',
-    project: 'https://mern-chat-app-frontend-three.vercel.app/'
+    image: expense,
+    heading: 'Expense Management System',
+    text: 'A MERN stack application with income and expense and management system with an intuitive web application.',
+    sourceCode: 'https://github.com/Jyotiranjankunda/Expense_management_system',
+    project: 'https://expense-management-system-lyart.vercel.app/login',
+  },
+  {
+    id: 2,
+    image: book,
+    heading: 'Book Explorer',
+    text: 'Seamless book discovery and exploration powered by the Google Books API.',
+    sourceCode: 'https://github.com/Jyotiranjankunda/Book-Explorer',
+    project: 'https://book-quest-app.vercel.app/',
   },
   {
     id: 2,
@@ -20,15 +29,16 @@ export const PROJECTS = [
     heading: 'You Tube Clone',
     text: 'YouTube clone built with React.js and Tailwind CSS, featuring real-time video fetching and filtering.',
     sourceCode: 'https://github.com/Jyotiranjankunda/You-Tube-clone',
-    project: 'https://youtube-clone-jk.netlify.app/'
+    project: 'https://youtube-clone-jk.netlify.app/',
   },
   {
     id: 3,
     image: wanderworld,
     heading: 'WanderWorld - A Tour and Travel Website',
     text: ' A tour and travel website offering seamless booking and travel planning experiences - built in React.',
-    sourceCode: 'https://github.com/Jyotiranjankunda/WanderWorld-A-Tour-and-Travel-Website',
-    project: 'https://wanderworld-jyotiranjan.netlify.app/'
+    sourceCode:
+      'https://github.com/Jyotiranjankunda/WanderWorld-A-Tour-and-Travel-Website',
+    project: 'https://wanderworld-jyotiranjan.netlify.app/',
   },
   {
     id: 4,
@@ -36,7 +46,7 @@ export const PROJECTS = [
     heading: 'Ecommerce - Frontend',
     text: 'A user-friendly e-commerce frontend application with comprehensive product management features',
     sourceCode: 'https://github.com/Jyotiranjankunda/E-commerce.github.io',
-    project: 'https://jyotiranjankunda.github.io/E-commerce.github.io'
+    project: 'https://jyotiranjankunda.github.io/E-commerce.github.io',
   },
   {
     id: 5,
@@ -44,7 +54,7 @@ export const PROJECTS = [
     heading: 'Ludo Game',
     text: 'An interactive multiplayer Ludo game built with HTML, CSS, and JavaScript.',
     sourceCode: 'https://github.com/Jyotiranjankunda/Ludo-game',
-    project: 'https://jyotiranjankunda.github.io/Ludo-game/'
+    project: 'https://jyotiranjankunda.github.io/Ludo-game/',
   },
   {
     id: 6,
@@ -52,6 +62,6 @@ export const PROJECTS = [
     heading: 'KeyBlaze - Speed Typing',
     text: 'A speed typing game designed to enhance typing skills through interactive challenges.',
     sourceCode: 'https://github.com/Jyotiranjankunda/KeyBlaze',
-    project: 'https://jyotiranjankunda.github.io/KeyBlaze/'
+    project: 'https://jyotiranjankunda.github.io/KeyBlaze/',
   },
-]
+];
